173ffa1b6e912eb84ee3e18e1d4a1fc6f2d8a236
Kms6666 <143037144+Kms6666@users.noreply.github.com>
2023-09-22 15:37:06 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+
+<form action="check.php" method="POST">
+
+	Username:<br>
+
+	<input type="text" name="username"><br>
+
+	Password <br>
+
+	<input type="text" name="password" ><br><br>
+
+	<input type="submit" value="Submit">
+
+</form>
